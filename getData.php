58e050dfd7cbc459dc3aa3398a028308d9a743e8
Korey Lo <koreylo9@gmail.com>
2016-03-15 12:46:16 -0700

    added ajax call so user can obtain information form the server

---
+++ getData.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Get Data From Student Grade Table</title>
+    <meta charset="utf-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+    <script>
+        var test;
+        function getData(){
+            $.ajax({
+                url: "index.php",
+                method: "POST",
+                dataType:'json',
+                data:{
+                    operation:"get"
+                },
+                success: function (response) {
+                    console.log(response);
+                    test = response;
+                }
+            });
+        }
+
+        $(document).ready(function(){
+            $("button").click(function(){
+                getData();
+            });
+        });
+
+    </script>
+</head>
+<body>
+<button type="button">Get Data</button>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,16 +1,38 @@
 <?php
     require_once('mysql_connect.php');
-    print("conn: "); print_r($conn);
-    $query = 'SELECT * FROM `Student Table`';
-    //WHERE user_id=101';
-    $result = mysqli_query($conn,$query);
-    print("<br><br><pre>".print_r($result).'</pre>');
-
-    if(mysqli_num_rows($result) > 0){
-        while($row = mysqli_fetch_assoc($result)){
-            $output = $row;
-            print("<br>".print_r($output));
-        }
+    if(!empty($_POST['operation'])){
+        $operation = $_POST['operation'];
     }
-
+    else{
+        $operation = 'none';
+    }
+    switch($operation){
+        case 'get':
+            //if(!empty($_POST['api_key']) && $_POST['api_key']=='tiggerrocks')
+            //print("conn: "); print_r($conn);
+            $query = 'SELECT * FROM `Student Table`';
+            //WHERE user_id=101';
+            $result = mysqli_query($conn, $query);
+            //print("<br><br><pre>".print_r($result).'</pre>');
+            $output = ['success' => false];
+            if (mysqli_num_rows($result) > 0) {
+                $output['success'] = true;
+                $output['data'] = [];
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $output['data'][] = $row;
+                    //print_r($output);
+                }
+            }
+            //print_r($output);
+            break;
+        case 'create':
+            break;
+        case 'delete':
+            break;
+        case 'update':
+            break;
+        default:
+            $output = ['success'=>'false','errors'=>['invalid operation']];
+    }
+    print(json_encode($output));
 ?>
